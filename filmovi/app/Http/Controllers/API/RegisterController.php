1008633a82238731d710cfef7c86173909c2acbc
Dejan Colic <dejan.colic99@gmail.com>
2021-05-19 20:49:48 +0200

    Register and login, sendResponse and sendError

--- filmovi/app/Http/Controllers/API/RegisterController.php
+++ filmovi/app/Http/Controllers/API/RegisterController.php
@@ -3,9 +3,48 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
+use Validator;
+use App\Http\Controllers\API\BaseController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
-    //
+    public function register(Request $request) {
+        $validator = Validator::make($request->all, [
+            'name'=>'required',
+            'email'=>'required|email',
+            'password'=>'required',
+            'c_password'=>'required|same:password'
+        ]);
+
+        if($validator->fails()) {
+            return $this->sendError('Validation error.', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] = $user->createToken('MyApp')->accessToken;
+        $success['name'] = $user->name;
+
+        return $this->sendResponse($success, 'User registered successfully');
+    }
+
+
+
+    public function login(Request $request) {
+        if(Auth::attempt(['email'=>$request->email, 'password'=>$request->password])) {
+            $user=Auth::user();
+            $success['token'] = $user->createToken('MyApp')->accessToken;
+            $success['name'] = $user->name;
+
+            return $this->sendResponse($success, 'User login successfull');
+        }else{
+            return $this->sendError('Unauthorised!', ['error'=>'Unauthorised!']);
+        }
+    }
+
 }
