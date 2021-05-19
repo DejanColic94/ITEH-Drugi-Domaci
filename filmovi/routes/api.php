5e187d2e371f1a1a825b463c93fe96df7e833087
Dejan Colic <dejan.colic99@gmail.com>
2021-05-19 19:13:29 +0200

    added controllers

--- filmovi/routes/api.php
+++ filmovi/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\MovieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login', [RegisterController::class, 'login']);
+
+// rute za pristup resursima, user mora da bude autentifikovan
+Route::middleware('auth:api')->group(function() {
+    Route::resource('movies', MovieController::class);
 });
